932d4122012f91340abeaa482fbba7971e19cff0
K198723 <k198723orikasa@gmail.com>
2022-03-27 13:38:10 +0900

    共通CSS適用、商品詳細ページ実装

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,28 +1,48 @@
 export interface Product {
     id: number,
     name: string,
-    description: string
+    description: string,
+    proce: number,
+    heading1: string,
+    heading2: string,
+    heading3: string
 }
 
 export const products = [
     {
         id: 0,
         name: 'iPhone',
-        description: 'Apple Inc.'
+        description: 'Apple Inc.',
+        price: 700,
+        heading1: '最高のパフォーマンス',
+        heading2: 'Best Performance',
+        heading3: '性能真好！'
     },
     {
         id: 1,
         name: 'Xperia',
-        description: 'From Sony.'
+        description: 'From Sony.',
+        price: 500,
+        heading1: '最高のパフォーマンス',
+        heading2: 'Best Performance',
+        heading3: '性能真好！'
     },
     {
         id: 2,
         name: 'Galaxy',
-        description: 'From Samsung.'
+        description: 'From Samsung.',
+        price: 600,
+        heading1: '最高のパフォーマンス',
+        heading2: 'Best Performance',
+        heading3: '性能真好！'
     },
     {
         id: 3,
         name: 'GooglePhone',
-        description: 'From Google.'
+        description: 'From Google.',
+        price: 400,
+        heading1: '最高のパフォーマンス',
+        heading2: 'Best Performance',
+        heading3: '性能真好！'
     }
 ]
